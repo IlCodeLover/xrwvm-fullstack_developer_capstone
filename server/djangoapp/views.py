7d3102aa5cd5414282beda41ca771f254a8837ff
IlCodeLover <ilcodelover@gmail.com>
2025-01-04 11:08:23 +0100

    [Flake8] fixing views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -64,15 +64,22 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name,
-                                        last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username, first_name=first_name, last_name=last_name,
+            password=password, email=email
+        )
 
         # login the user and redirect to list page
         login(request, user)
-        data = {"user": username, "status": "Authenticated"}
+        data = {
+            "user": username,
+            "status": "Authenticated"
+        }
         return JsonResponse(data)
     else:
-        data = {"userName": username, "error": "Already registered"}
+        data = {
+            "userName": username,
+            "error": "Already registered"}
         return JsonResponse(data)
 
 
@@ -86,7 +93,10 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         # get car model info and append additional car make's name from via Foreign key
-        cars.append({'CarModel': car_model.name, 'CarMake': car_model.car_make.name})  # one CarMake has many CarModel
+        cars.append({
+            'CarModel': car_model.name,
+            'CarMake': car_model.car_make.name
+        })  # one CarMake has many CarModel
 
     return JsonResponse({"CarModels": cars})
 
@@ -108,12 +118,18 @@ def get_dealer_reviews(request, dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            print(response)
+            response = analyze_review_sentiments(
+                review_detail['review'])
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
+        return JsonResponse({
+            "status": 200,
+            "reviews": reviews
+        })
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
 
 
 # Create a `get_dealer_details` view to render the dealer details
@@ -121,9 +137,15 @@ def get_dealer_details(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+        return JsonResponse({
+            "status": 200,
+            "dealer": dealership
+        })
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
 
 
 # Create a `add_review` view to submit a review
@@ -134,6 +156,12 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({
+            "status": 403,
+            "message": "Unauthorized"
+        })
